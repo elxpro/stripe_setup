4743a4c4220ac06b4228eb97b1b18b9ab6b5df17
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-07-02 16:02:04 -0300

    testing the lv

--- lib/stripe_setup_web/live/pricing_live/product_component.ex
+++ lib/stripe_setup_web/live/pricing_live/product_component.ex
@@ -3,13 +3,16 @@ defmodule StripeSetupWeb.PricingLive.ProductComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="hover:bg-blue-200 bg-blue-100 transition-all p-2 rounded-lg">
+    <div id={@id} class="hover:bg-blue-200 bg-blue-100 transition-all p-2 rounded-lg">
       <p class="mb-1 text-xs font-semibold tracking-wide text-gray-500 uppercase">
         <%= @product.stripe_product_name %>
       </p>
       <h1 class="mb-2 text-4xl font-bold leading-tight text-gray-900 md:font-extrabold">
         <%= @product.plan.amount %>
-        <span class="text-2xl font-medium text-gray-600"> per month</span>
+
+        <span class="text-2xl font-medium text-gray-600">
+          per <%= @product.plan.stripe_plan_name %>
+        </span>
       </h1>
       <p class="text-lg text-gray-600 mb-12">
         ....
---
+++ test/stripe_setup_web/live/pricing_live/index_test.exs
@@ -0,0 +1,103 @@
+defmodule StripeSetupWeb.PricingLive.IndexTest do
+  use StripeSetupWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  import MockStripe, only: [token: 0]
+  alias StripeSetup.Billing
+
+  setup [:setup_products_and_plans]
+
+  describe "render/1" do
+    test "renders log in page", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/pricings")
+      assert has_element?(lv, "h1", "Plans & Pricing")
+      assert has_element?(lv, "h1", "$9.00")
+      assert has_element?(lv, "h1", "$19.00")
+      assert has_element?(lv, "h1>span", "per month")
+      refute has_element?(lv, "h1>span", "per year")
+      # open_browser(lv, &System.cmd("wslview", [&1]))
+    end
+  end
+
+  describe "handle_params/3" do
+    test "renders log in page", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/pricings")
+      assert has_element?(lv, "h1>span", "per month")
+      refute has_element?(lv, "h1>span", "per year")
+
+      lv
+      |> element("a[href=\"/pricings?interval=year\"]")
+      |> render_click()
+
+      assert_patched(lv, "/pricings?interval=year")
+
+      refute has_element?(lv, "h1>span", "per month")
+      assert has_element?(lv, "h1>span", "per year")
+
+      lv
+      |> element("a[href=\"/pricings?interval=month\"]")
+      |> render_click()
+
+      assert has_element?(lv, "h1>span", "per month")
+      refute has_element?(lv, "h1>span", "per year")
+
+      assert_patched(lv, "/pricings?interval=month")
+    end
+  end
+
+  def setup_products_and_plans(_) do
+    products_data()
+    |> Enum.each(fn %{
+                      stripe_id: stripe_id,
+                      stripe_product_name: stripe_product_name,
+                      plans: plans
+                    } ->
+      {:ok, product} =
+        Billing.create_product(%{stripe_id: stripe_id, stripe_product_name: stripe_product_name})
+
+      plans
+      |> Enum.each(fn plan_attrs ->
+        Billing.create_plan(product, plan_attrs)
+      end)
+    end)
+
+    :ok
+  end
+
+  defp products_data do
+    [
+      %{
+        plans: [
+          %{
+            amount: 9900,
+            stripe_id: "price_#{token()}",
+            stripe_plan_name: "year"
+          },
+          %{
+            amount: 900,
+            stripe_id: "price_#{token()}",
+            stripe_plan_name: "month"
+          }
+        ],
+        stripe_id: "prod_#{token()}",
+        stripe_product_name: "Standard Plan"
+      },
+      %{
+        plans: [
+          %{
+            amount: 1900,
+            stripe_id: "price_#{token()}",
+            stripe_plan_name: "month"
+          },
+          %{
+            amount: 19900,
+            stripe_id: "price_#{token()}",
+            stripe_plan_name: "year"
+          }
+        ],
+        stripe_id: "prod_#{token()}",
+        stripe_product_name: "Premium Plan"
+      }
+    ]
+  end
+end
