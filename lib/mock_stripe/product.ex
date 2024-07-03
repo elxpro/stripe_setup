3cdd5c0be29d8cd1038227317348b2d87b1af995
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-07-03 16:20:12 -0300

    mock main stuffs

---
+++ lib/mock_stripe/product.ex
@@ -0,0 +1,34 @@
+defmodule MockStripe.Product do
+  alias MockStripe.List
+
+  defstruct [
+    :id,
+    :created,
+    :name,
+    :object,
+    :updated
+  ]
+
+  def retrieve, do: retrieve("prod_#{MockStripe.token()}")
+
+  def retrieve("prod_" <> _ = stripe_id) do
+    %__MODULE__{
+      id: stripe_id,
+      object: "product",
+      created: 1_597_085_304,
+      name: "pumpkin 5",
+      updated: 1_719_747_375
+    }
+  end
+
+  def list(_ \\ %{}) do
+    {:ok,
+     %List{
+       data: [retrieve()],
+       object: "list",
+       has_more: false,
+       total_count: nil,
+       url: "/v1/products"
+     }}
+  end
+end
